7d410388ae23084045f02d4e0d49f71e9beb4306
maythingo <maythingo@aim.com>
2021-04-03 22:03:07 -0700

    adding arrays for characters, and fixed password length if else statments

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 // Assignment Code
+var onlyUpper = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+var onlyLower = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+var onlySpecial = ['#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','['];
+var onlyNumeric = [0,1,2,3,4,5,6,7,8,9];
+
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
@@ -14,11 +19,16 @@ generateBtn.addEventListener("click", writePassword);
 
 //password prompts
 function createPassword() {
-var passwordLength = prompt("Please enter the desired password length of at least 8 characters and no more than 128 characters.");
-  if (passwordLength<8 || passwordLength >129 || passwordLength === ""){
-    alert("Error: You do not have at least 8 characters or no more than 128 characters!");
-    return passwordLength;
+var passwordLength = parseInt(prompt("Please enter the desired password length of at least 8 characters and no more than 128 characters."));;
+  if (passwordLength<8){
+    window.alert("Error: Pick a length with least 8 characters!");
+    return;
   }
+  else if (passwordLength>128){
+    window.alert("Error: Pick a length less than 128 characters!");
+    return;
+  }
+
 var lowerCase = confirm("Do you want lowercase characters in your password? ");
 var upperCase = confirm("Do you want uppercase characters in your password?");
 var number = confirm("Do you want numbers in your password?");
