2057c37fb1408ee00cc35fb74d9da188559eefef
maythingo <maythingo@aim.com>
2021-04-03 21:26:24 -0700

    trying to add password length function, not successful

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  
   passwordText.value = password;
 }
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 //password prompts
-var passwordLength = ("Please enter a password with at least 8 characters and no more than 128 characters.");
-var lowerCase = ("Do you want lowercase characters in your password? ");
-var UpperCase = ("Do you want uppercase characters in your password?");
-var number = ("Do you want numbers in your password?");
-var specialChar = ("Do you want special characters in your password?");
+function createPassword() {
+var passwordLength = prompt("Please enter the desired password length of at least 8 characters and no more than 128 characters.");
+  if (passwordLength<8 || passwordLength >129 || passwordLength === ""){
+    alert("Error: You do not have at least 8 characters or no more than 128 characters!");
+    return passwordLength;
+  }
+var lowerCase = confirm("Do you want lowercase characters in your password? ");
+var upperCase = confirm("Do you want uppercase characters in your password?");
+var number = confirm("Do you want numbers in your password?");
+var specialChar = confirm("Do you want special characters in your password?");
+}
+
